f59ca8b686335d2e7862428a8c87d19f3b482c30
yash pawar <45911343+yashpwr@users.noreply.github.com>
2022-07-17 21:07:31 +0530

    loading data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,18 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetata } = require('./models/planets.model')
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+async function startServer() {
+    await loadPlanetata();
+}
+
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}...`);
 })
+
+startServer();
