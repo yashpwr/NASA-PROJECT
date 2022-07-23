a23095037bae5b89cbcb5186ed9ed653b49bde5f
yash pawar <45911343+yashpwr@users.noreply.github.com>
2022-07-23 22:11:58 +0530

    Lauches Model added

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const planetsRouter = require('./routes/planets/planets.router')
-
+const planetsRouter = require('./routes/planets/planets.router');
+const morgan = require('morgan');
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
