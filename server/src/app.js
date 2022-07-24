1375885842523b088d6c5f3a12aec72c321bc707
yash pawar <45911343+yashpwr@users.noreply.github.com>
2022-07-24 10:09:18 +0530

    GET - Launches API integrated

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/lauches/launches.router');
 const morgan = require('morgan');
 const app = express();
 
@@ -9,14 +10,15 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+app.use(launchesRouter);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'test', 'index.html'))
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 });
 
 module.exports = app;
