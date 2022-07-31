15498214357d5a1154902ddd865ced8d6e580fc5
yash pawar <45911343+yashpwr@users.noreply.github.com>
2022-07-31 13:18:44 +0530

    creating and insterting Documents

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path  = require('path');
 const { parse } = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const HabitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -15,9 +17,12 @@ function loadPlanetata() {
             comment: '#',
             columns: true,
         }))
-        .on('data', (data) => {
+        .on('data', async (data) => {
             if(isHabitablePlanet(data)){
-                HabitablePlanets.push(data);
+                // HabitablePlanets.push(data);
+                await planets.create({
+                    keplerName: data.kepler_name
+                });
             }
         })
         .on('error', (err) => {
