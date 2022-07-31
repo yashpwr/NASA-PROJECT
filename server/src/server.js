7123e0eb3f28743c4b5c8175b7ebf9294a4e5890
yash pawar <45911343+yashpwr@users.noreply.github.com>
2022-07-31 12:41:15 +0530

    MongoDB connectivity using Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,27 @@
 const http = require('http');
-
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetata } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://yashpwr:yashpwr@nasacluster.pkbtk.mongodb.net/?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB Connection is ready');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetata();
 }
 
